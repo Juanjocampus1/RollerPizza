492c77f62ec903ee16d659d47bf6586a1f880846
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-02-19 01:01:56 +0100

    mejoramos responsive, reformulamos el crea tu pizza y separamos en archivos js la info de las pizzas

---
+++ src/components/Utils/EntrantesPostres.js
@@ -0,0 +1,68 @@
+export const entrantes = [
+    {
+        title: "alitas de pollo",
+        description: "jugosas y con un toque picante",
+        quantity: 5,
+        image: "/images/alitas.jpg",
+    },
+    {
+        title: "Nuggets de pollo",
+        description: "Crujientes al estilo americano",
+        quantity: 5,
+        image: "/images/nachos.jpg",
+    },
+    {
+        title: "croquetas",
+        description: "¡de toda la vida!",
+        quantity: 5,
+        image: "/images/croquetas.jpg",
+    },
+    {
+        title: "Fingers de queso",
+        description: "suaves y cremosos queso",
+        quantity: 5,
+        image: "/images/fingers.jpg",
+    },
+    {
+        title: "Tequeños",
+        description: "Queso gouda envuelto en pan tostado",
+        quantity: 3,
+        image: "/images/tequeños.jpg",
+    },
+    {
+        title: "Pan de ajo",
+        description: "Delicioso, cremoso. Italiano",
+        quantity: 3,
+        image: "/images/Pan.jpg",
+    },
+    {
+        title: "Delicias de pollo",
+        description: "Delicias de pollo con toque picante",
+        quantity: 4,
+        image: "/images/delicias.jpg",
+    },
+    {
+        title: "Mac & Cheese",
+        description: "Delicioso, Americano",
+        quantity: 5,
+        image: "/images/Mac.jpg",
+    }
+];
+
+export const Postres = [
+    {
+        name: "Helado de chocolate / vainilla",
+        description: "Helado del mas delcioso chocolate y la mas suave vainilla",
+        image: "/images/chocolate.jpg",
+    },
+    {
+        name: "Helado de fresa / nata",
+        description: "Helado de fresa y la mas deliciosa nata",
+        image: "/images/fresa.jpg",
+    },
+    {
+        name: "Tequeños de chocolate",
+        description: "2 Tequeños de chocolate",
+        image: "/images/tequeños.jpg",
+    }  
+];
