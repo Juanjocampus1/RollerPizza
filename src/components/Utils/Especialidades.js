181ff7388cd4994762efbaf3440a12102f4e0e19
juanjo <juanjo@gmail.com>
2025-02-21 21:05:56 +0100

    terminamos de pulir todo para produccion, solo falta terminar con las imagenes

--- src/components/Utils/Especialidades.js
+++ src/components/Utils/Especialidades.js
@@ -2,112 +2,112 @@ export const PizzaClasica = [
     {
         Name: "JUNIOR´S PIZZA",
         Ingredients: "Salsa de tomate, mozzarella, jamón, salchichas y extra de queso",
-        Image: "public/Pizzas/junior_s.png",
+        Image: "public/Pizzas/images/junior_s.webp",
         PizzaDescription: "pizza junior",
         message: "¡La favorita de los más peques!"
     },
     {
         Name: "Pollo a la parrilla",
         Ingredients: "Salsa de tomate, mozzarella, pollo marinado, champiñones y maiz",
-        Image: "public/Pizzas/pollo_parrilla.png",
+        Image: "public/Pizzas/images/pollo_parrilla.webp",
         PizzaDescription: "pizza pollo a la parrilla",
         message: "¡Una delicia para los amantes del pollo!"
     },
     {
         Name: "Tex Mex",
         Ingredients: "Salsa mejicana, queso cheddar, nachos, ternera y jalapeños",
-        Image: "public/Pizzas/tex_mex.png",
+        Image: "public/Pizzas/images/tex-mex-artistica.webp",
         PizzaDescription: "pizza tex mex",
         message: "¡Para los amantes de la comida mejicana!"
     },
     {
         Name: "De la casa",
         Ingredients: "Salsa de tomate, mozzarella, bacon, ternera, champiñones y pimiento natural",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/delacasa-diagonal.webp",
         PizzaDescription: "pizza de la casa",
         message: "¡La especialidad de la casa!"
     },
     {
         Name: "4 quesos",
         Ingredients: "Salsa de tomate, mozzarella, bacon, ternera, champiñones y pimiento natural",
-        Image: "public/Pizzas/de_la_casa.png",
-        PizzaDescription: "pizza de la casa",
+        Image: "public/Pizzas/images/4-quesos-diagonal.webp",
+        PizzaDescription: "pizza 4 quesos",
         message: "¡La clasica que conoces!"
     },
     {
         Name: "Mejicana",
         Ingredients: "Salsa mejcana, mozzarella, pimiento, cebolla y jalapeños",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/mejicana-diagonal.webp",
         PizzaDescription: "pizza Mejicana",
         message: "¡Para los amantes de lo picante!"
     },
     {
         Name: "Cheddar cream",
         Ingredients: "Salsa carbonara, queso, queso cheddar, bacon y salchichas",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/cheddar-cream-diagonal.webp",
         PizzaDescription: "pizza Cheddar cream",
         message: "¡Para los amantes del queso!"
     },
     {
         Name: "Queso de cabra",
         Ingredients: "Salsa carbonara, mozzarella, queso de cabra y cebolla caramelizada",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/queso-de-cabra-artistica.webp",
         PizzaDescription: "pizza Queso de cabra",
         message: "¡los sabores mas cogenian!"
     },
     {
         Name: "Onion crispie",
         Ingredients: "Salsa de tomate, queso cheddar, bacon, cebolla crujiente y topping de salsa barbacoa",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/onion-crispie.webp",
         PizzaDescription: "pizza Onion crispie",
         message: "¡todos los sabores en una pizza!"
     },
     {
         Name: "Carbonara",
         Ingredients: "Salsa carbonara, mozzarella, bacon, champiñones y cebolla natural",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/carbonara-cartel.webp",
         PizzaDescription: "pizza Carbonara",
         message: "¡La clasica que conoces!"
     },
     {
         Name: "Vegetal",
         Ingredients: "Salsa de tomate, mozzarella, tomate natural, champiñones y aceitunas negras",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/vegetal-diagonal.webp",
         PizzaDescription:"pizza Vegetal",
         message: "¡Para los amantes de lo natural!"
     },
     {
         Name: "Donner pizza",
         Ingredients: "Salsa de kebab, mozzarella, tomate natural y pollo kebab",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/pollo_parrilla.webp",
         PizzaDescription: "Donner pizza",
         message: "¡Lo que triunfa en turquia!"
     },
     {
         Name: "Mediterranea",
         Ingredients: "Salsa de tomate, mozzarella, atun, tomate natural y aceitunas negras",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/pollo_parrilla.webp",
         PizzaDescription: "pizza Mediterranea",
         message: "¡lo mejor del mediterraneo!"
     },
     {
         Name: "Ideal",
         Ingredients: "Salsa de tomate, mozzarella, jamon de york, chmapiñones y tomate natural",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/pollo_parrilla.webp",
         PizzaDescription: "pizza Ideal",
         message: "¡El nombre lo dice todo!"
     },
     {
         Name: "Barbacoa",
         Ingredients: "Salsa barbacoa, mozzarella, bacon, ternera y cebolla natural o maiz",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/pollo_parrilla.webp",
         PizzaDescription: "pizza barbacoa",
         message: "¡Lo mas americano!"
     },
     {
         Name: "Sweet pepperoni",
         Ingredients: "Salsa bbq/salsa tomate, mozzarella, pepperoni, topping de miel",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/pollo_parrilla.webp",
         PizzaDescription: "pizza Sweet pepperoni",
         message: "¡El toque dulce que te sorprendera!"
     }
@@ -117,35 +117,35 @@ export const PizzaPremiun = [
     {
         Name: "Hot chicken",
         Ingredients: "Salsa barbacoa, mozzarella, pepperoni, pollo marinado y ternera",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/pollo_parrilla.webp",
         PizzaDescription: "pizza hot chicken",
         message: "¡el toque marinado que te sorprendera!"
     },
     {
         Name: "Cremosa Miel y Mostaza",
         Ingredients: "Salsa carbonara, queso cheddar, ternera, pollo marinado y topping de miel y mostaza",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/pollo_parrilla.webp",
         PizzaDescription: "pizza Cremosa Miel y Mostaza",
         message: "¡el dulce y el salado en una pizza!"
     },
     {
         Name: "Pizza burguer",
         Ingredients: "Salsa burguer, queso cheddar, bacon y ternera",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/pollo_parrilla.webp",
         PizzaDescription: "pizza burguer",
         message: "¡el sabor de la burguer en una pizza!"
     },
     {
         Name: "Pulled pork",
         Ingredients: "Salsa ranchera, mozzarella, pulled pork, y cebolla caramelizada",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/pollo_parrilla.webp",
         PizzaDescription: "pizza Pulled pork",
         message: "¡la carne mas tierna en una pizza!"
     },
     {
         Name: "Emmy´s pizza",
         Ingredients: "Salsa emmy, mozzarella, cebolla, bacon y ternera",
-        Image: "public/Pizzas/de_la_casa.png",
+        Image: "public/Pizzas/images/pollo_parrilla.webp",
         PizzaDescription: "pizza Emmy´s",
         message: "¡lo mejor de la casa!"
     }
