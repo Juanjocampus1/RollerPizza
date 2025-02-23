f75d19e30688347564e46940f1bda46df950c82d
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-02-24 00:05:28 +0100

    corregimos  errores con la visualizacion de las imagenes

--- src/components/Utils/EntrantesPostres.js
+++ src/components/Utils/EntrantesPostres.js
@@ -3,49 +3,49 @@ export const entrantes = [
         title: "alitas de pollo",
         description: "jugosas y con un toque picante",
         quantity: 5,
-        image: "public/Pizzas/images/alitas-singrasa.webp",
+        image: "/Pizzas/images/alitas-singrasa.webp",
     },
     {
         title: "Nuggets de pollo",
         description: "Crujientes al estilo americano",
         quantity: 5,
-        image: "public/Pizzas/images/nugets-arreglados.webp",
+        image: "/Pizzas/images/nugets-arreglados.webp",
     },
     {
         title: "croquetas",
         description: "¡de toda la vida!",
         quantity: 5,
-        image: "public/Pizzas/images/croquetas-diagonal.webp",
+        image: "/Pizzas/images/croquetas-diagonal.webp",
     },
     {
         title: "Fingers de queso",
         description: "suaves y cremosos queso",
         quantity: 5,
-        image: "public/Pizzas/images/fingers-arreglados.webp",
+        image: "/Pizzas/images/fingers-arreglados.webp",
     },
     {
         title: "Tequeños",
         description: "Queso gouda envuelto en pan tostado",
         quantity: 3,
-        image: "public/Pizzas/images/tequenos-img.webp",
+        image: "/Pizzas/images/tequenos-img.webp",
     },
     {
         title: "Pan de ajo",
         description: "Delicioso, cremoso. Italiano",
         quantity: 3,
-        image: "public/Pizzas/images/pan-de-ajo-arreglado.webp",
+        image: "/Pizzas/images/pan-de-ajo-arreglado.webp",
     },
     {
         title: "Delicias de pollo",
         description: "Delicias de pollo con toque picante",
         quantity: 4,
-        image: "public/Pizzas/images/delicias-de-pollo.webp",
+        image: "/Pizzas/images/delicias-de-pollo.webp",
     },
     {
         title: "Mac & Cheese",
         description: "Delicioso, Americano",
         quantity: 5,
-        image: "public/Pizzas/images/mac-cheese.webp",
+        image: "/Pizzas/images/mac-cheese.webp",
     }
 ];
 
@@ -53,16 +53,16 @@ export const Postres = [
     {
         name: "Helado de chocolate / vainilla",
         description: "Helado del mas delcioso chocolate y la mas suave vainilla",
-        image: "public/Pizzas/images/alitas-singrasa.webp",
+        image: "/Pizzas/images/alitas-singrasa.webp",
     },
     {
         name: "Helado de fresa / nata",
         description: "Helado de fresa y la mas deliciosa nata",
-        image: "public/Pizzas/images/alitas-singrasa.webp",
+        image: "/Pizzas/images/alitas-singrasa.webp",
     },
     {
         name: "Tequeños de chocolate",
         description: "2 Tequeños de chocolate",
-        image: "public/Pizzas/images/alitas-singrasa.webp",
+        image: "/Pizzas/images/alitas-singrasa.webp",
     }  
 ];
