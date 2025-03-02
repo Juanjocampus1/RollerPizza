07496a820c4ff8d1919c27533a689bca0ef8c86b
Juanjo <juanjose.acebedolara.23@campusfp.es>
2025-03-02 10:40:35 +0100

    añadimos informacion faltante

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+	purge: {
+		content: [
+			'./src/**/*.html',
+			'./src/**/*.astro',
+			'./src/**/*.js',
+		  // Asegúrate de incluir todos los archivos relevantes
+		],
+		},
+		theme: {
+			screens: {
+				'sm': '640px',
+				'md': '768px',
+				'lg': '1024px',
+				'xl': '1280px',
+				'2xl': '1536px',
+			},
 		extend: {
-		  	keyframes: {
+			keyframes: {
 				fadeInDrop: {
 					"0%": { opacity: 0, transform: "translateY(-20px)" },
 			    	"100%": { opacity: 1, transform: "translateY(0)" }
 				}
-		  	},
-		  	animation: {
+			},
+			animation: {
 				fadeInDrop: "fadeInDrop 1s ease-out forwards"
 			}
 		}
